a74be3ce1cc1892a523e209361ee1fe10bf2a8ab
Poya Faraji <poya.faraji@outlook.com>
2026-08-01 22:11:25 +0330

    [INIT] 09 - Dev Tools Domination

---
+++ 09 - Dev Tools Domination/src/scripts/app.js
@@ -0,0 +1,37 @@
+const pElem = document.querySelector("#break-dowm");
+
+pElem.addEventListener("click", makeGreen);
+
+const dogs = [
+  { name: "Snickers", age: 2 },
+  { name: "hugo", age: 8 },
+];
+
+function makeGreen() {
+  pElem.style.color = "#BADA55";
+  pElem.style.fontSize = "50px";
+}
+
+// Regular
+
+// Interpolated
+
+// Styled
+
+// warning!
+
+// Error :|
+
+// Info
+
+// Testing
+
+// clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// counting
+
+// timing
