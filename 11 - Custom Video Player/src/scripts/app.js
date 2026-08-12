666c6877a26829aa7adec400744f8e526864bcfe
Poya Faraji <poya.faraji@outlook.com>
2026-08-12 12:02:12 +0330

    [ADD] play and stop logic

---
+++ 11 - Custom Video Player/src/scripts/app.js
@@ -0,0 +1,27 @@
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+
+const progress = player.querySelector(".progress");
+
+const progressBar = player.querySelector(".progress__filled");
+
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+
+const ranges = player.querySelectorAll(".player__slider");
+
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+}
+
+function updateButton() {
+  const icon = video.paused ? "▶" : "⏸";
+  toggle.textContent = icon;
+}
+
+toggle.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+
+// handle button update based on pause
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
